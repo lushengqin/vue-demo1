d7567e38ae6db3e20a04830fc3aae5e87bb66673
lushengqin <2015941587@qq.com>
2021-02-25 10:35:38 +0800

    错误提交

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    {{n}}
+    <button @click="btn">点击按钮+1</button>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      n:0
+    }
+  },
+  methods:{
+    btn(){
+      this.n+=1
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+console.log(window.Vue)
+// import Vue from 'vue'
+// import App from './App.vue'
+
+// Vue.config.productionTip = false
+
+// // new Vue({
+// //   render: h => h(App),
+// // }).$mount('#app')
+
+
+// // const vm = new Vue({  //new实例化
+// //   render:h=> h(App),
+// // }).$mount('#app')
+
+// // const $div = new jQuery('#xxx')  //jQuery实例化  jQuery可以省略前面的new 
+// // const $div = jQuery('#xxx') // const $div = $('#xxx) //所以我们也可以写成这两种方式 
+// new Vue({
+//   el:'#app'
+// })
